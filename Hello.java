37450bf13fc4cc833b861ae1ad0694787f6695c3
Kartikeya <kartikeya.anjul@gmail.com>
2025-02-10 17:34:42 +0530

    initialization of vars

--- Hello.java
+++ Hello.java
@@ -2,7 +2,8 @@ class Hello
 {
     public static void main (String args[])
     {
+        int num = 10;
         System.out.println("Hello world");
-        System.out.println(8+9);
+        System.out.println(num);
     }
 }
