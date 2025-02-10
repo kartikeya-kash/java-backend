0fdc8ff6fd28713f3d7b272e8604761d4e3be04b
Kartikeya <kartikeya.anjul@gmail.com>
2025-02-10 17:49:07 +0530

    data types

--- Hello.java
+++ Hello.java
@@ -2,8 +2,28 @@ class Hello
 {
     public static void main (String args[])
     {
-        int num = 10;
-        System.out.println("Hello world");
-        System.out.println(num);
+        // data types
+        int n1 = 9;
+        short n2 = 2;
+        long n3 = 923423;
+        byte n4 = 122;
+
+        double  n5 = 8.3;
+        float n6= 8.3f;
+
+        char c = 'A';
+
+        boolean b = true;
+
+        System.out.println(n1);
+        System.out.println(n2);
+        System.out.println(n3);
+        System.out.println(n4);
+        System.out.println(n5);
+        System.out.println(n6);
+        System.out.println(c);
+        System.out.println(b);
+        
+
     }
 }
