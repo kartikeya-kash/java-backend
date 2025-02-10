6e3d5805b60c1b7e34c81fc2c4d85d7ff07d4723
Kartikeya <kartikeya.anjul@gmail.com>
2025-02-10 21:30:05 +0530

    promotion in conversion

--- Hello.java
+++ Hello.java
@@ -6,8 +6,12 @@ class Hello
         // byte b = 258 error as out of range
        // byte b = 127 ;
         int a = 257; //works
-        byte k = (byte)(a); // will store a%range in k
-        System.out.println(k); 
-        
+       // byte k = (byte)(a); // will store a%range in k
+        //System.out.println(k); 
+
+        //promotion 
+        byte k = 124;
+        int va = k*k; // here k*k will be promoted to int
+        System.out.println(va); 
     }
 }
