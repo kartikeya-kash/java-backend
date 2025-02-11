b5ce0a3261187b0eae4bab3291e8dbb38a87a981
Kartikeya <kartikeya.anjul@gmail.com>
2025-02-11 19:48:28 +0530

    class and obj

--- demo.java
+++ demo.java
@@ -1,5 +1,17 @@
 public class demo {
     public static void main(String [] args){
-        
+        claculator obj1 = new claculator();
+        obj1.add();
+    }
+}
+
+class claculator { // here we will design the stuff 
+
+    int a; // variables in class 
+
+    public int add() //method
+    {
+        System.out.println("in add ");
+        return 0;
     }
 }
