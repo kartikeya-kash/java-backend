78802132878037812bf736cc71c913e08a1874e4
Kartikeya <kartikeya.anjul@gmail.com>
2025-02-11 18:21:32 +0530

    ternary

--- Hello.java
+++ Hello.java
@@ -2,16 +2,10 @@ class Hello
 {
     public static void main (String args[])
     {
-        // type conversion
-        // byte b = 258 error as out of range
-       // byte b = 127 ;
-        int a = 257; //works
-       // byte k = (byte)(a); // will store a%range in k
-        //System.out.println(k); 
+       int result;
+       int x=2; 
 
-        //promotion 
-        byte k = 124;
-        int va = k*k; // here k*k will be promoted to int
-        System.out.println(va); 
+       result = x%2==0 ? 10 : 20 ; // if x%2==0 then reult = 10 else result = 20
+       System.out.println(result);
     }
 }
