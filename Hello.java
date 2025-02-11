1d577b6ad5b28e6b51a4013210f049ddb1965fc7
Kartikeya <kartikeya.anjul@gmail.com>
2025-02-11 19:42:48 +0530

    classes concept

--- Hello.java
+++ Hello.java
@@ -2,10 +2,10 @@ class Hello
 {
     public static void main (String args[])
     {
-       int result;
-       int x=2; 
-
-       result = x%2==0 ? 10 : 20 ; // if x%2==0 then reult = 10 else result = 20
-       System.out.println(result);
+        int i=0;
+       while (true){
+        System.out.println("hi "+ i);
+        i++;
+       }
     }
 }
